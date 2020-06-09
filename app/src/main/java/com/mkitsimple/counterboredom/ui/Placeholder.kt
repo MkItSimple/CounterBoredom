6c977eeddb1aeace2be174fb20c2b15dba802837
MkItSimple <choanania1@gmail.com>
2020-06-09 20:48:18 +0800

    Initial commit

---
+++ app/src/main/java/com/mkitsimple/counterboredom/ui/Placeholder.kt
@@ -0,0 +1,2 @@
+package com.mkitsimple.counterboredom.ui
+
---
+++ app/src/main/java/com/mkitsimple/counterboredom/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mkitsimple.counterboredom.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mkitsimple.counterboredom.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
